dc89bc234cbd2c1a770d6de09d41a05ceb3fb089
Jake Soenneker <jake@soenneker.com>
2026-05-11 09:37:04 -0500

    additional values

--- src/Soenneker.Quark.Enums.DisplayTypes/DisplayEnum.cs
+++ src/Soenneker.Quark.Enums.DisplayTypes/DisplayEnum.cs
@@ -9,11 +9,20 @@ public sealed partial class DisplayEnum
     public static readonly DisplayEnum Inline = new("inline");
     public static readonly DisplayEnum InlineBlock = new("inline-block");
     public static readonly DisplayEnum Block = new("block");
+    public static readonly DisplayEnum FlowRoot = new("flow-root");
     public static readonly DisplayEnum Flex = new("flex");
     public static readonly DisplayEnum InlineFlex = new("inline-flex");
     public static readonly DisplayEnum Grid = new("grid");
     public static readonly DisplayEnum InlineGrid = new("inline-grid");
     public static readonly DisplayEnum Table = new("table");
+    public static readonly DisplayEnum TableCaption = new("table-caption");
+    public static readonly DisplayEnum TableColumn = new("table-column");
+    public static readonly DisplayEnum TableColumnGroup = new("table-column-group");
+    public static readonly DisplayEnum TableFooterGroup = new("table-footer-group");
+    public static readonly DisplayEnum TableHeaderGroup = new("table-header-group");
+    public static readonly DisplayEnum TableRowGroup = new("table-row-group");
     public static readonly DisplayEnum TableCell = new("table-cell");
     public static readonly DisplayEnum TableRow = new("table-row");
+    public static readonly DisplayEnum Contents = new("contents");
+    public static readonly DisplayEnum ListItem = new("list-item");
 }
